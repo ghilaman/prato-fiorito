b4289d496ca98ae65c8a028bfab749a132294809
Fabio Rigoletti <ghilardi99@gmail.com>
2015-12-19 15:24:08 +0100

    creata  funzionw richiesta

--- Prato fiorito/Program.cs
+++ Prato fiorito/Program.cs
@@ -11,34 +11,65 @@ namespace Prato_fiorito
         static int n_mine = 9;
         static int colonne = 9;
         static int righe = 9;
+        static int colonnautente;
+        static int rigautente;
         static int[,] campo = new int[righe, colonne];
-         
+
         static void Main(string[] args)
         {
-            
-
-
+            campo = preparazioneCampo();
+            richiesta();
         }
 
-        static int [,] preparazioneCampo()
+        static int[,] preparazioneCampo()
         {
             Random mine = new Random();
-            
-            for (int i = 0; i < 9; i++)
+
+            for (int i = 0; i < righe; i++)
             {
                 int rigamina;
                 int colonnamina;
                 do
                 {
-                    rigamina = mine.Next(1, 9);
-                    colonnamina = mine.Next(1, 9);
-                } while (campo[rigamina,colonnamina] == 1);
-                campo[rigamina, colonnamina] = 1;   
+                    rigamina = mine.Next(1, righe);
+                    colonnamina = mine.Next(1, colonne);
+                } while (campo[rigamina, colonnamina] == 1);
+                campo[rigamina, colonnamina] = 1;
             }
             return campo;
         }
-        
-        
-    
+
+        static void richiesta()
+        {
+            bool verifica;
+            do
+            {
+                Console.WriteLine("dammi la riga");
+                verifica = Int32.TryParse(Console.ReadLine(), out rigautente);
+                if (!verifica || rigautente < 1 || rigautente > 9)
+                {
+                    Console.WriteLine("inserire un valore corretto");
+                }
+
+            } while (!verifica || rigautente > 9|| rigautente<1);
+
+            bool verifica2;
+            do
+            {
+                Console.WriteLine("dammi la colonna");
+                verifica2 = Int32.TryParse(Console.ReadLine(), out rigautente);
+                if (!verifica2 || rigautente < 1 || rigautente > 9)
+                {
+                    Console.WriteLine("inserire un valore corretto");
+                }
+                    
+            } while (!verifica2 || colonnautente > 9|| colonnautente<1);
+
+        }
     }
+
+
+
+
 }
+
